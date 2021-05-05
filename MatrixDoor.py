379843e80bfef3f014fcf7a0f7341765a7074a67
Blaqsk8 <sonofstilgar@gmail.com>
2021-05-05 10:10:16 -0400

    Create MatrixDoor.py

---
+++ MatrixDoor.py
@@ -0,0 +1,34 @@
+import board
+import displayio
+import busio
+from digitalio import DigitalInOut
+from analogio import AnalogIn
+import neopixel
+import adafruit_adt7410
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_bitmap_font import bitmap_font
+from adafruit_display_text.label import Label
+from adafruit_button import Button
+import adafruit_touchscreen
+import adafruit_minimqtt.adafruit_minimqtt as MQTT
+ 
+# ------------- WiFi ------------- #
+ 
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+ 
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+ 
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
